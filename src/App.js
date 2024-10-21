729ad0f0f8fcf17ec520020ae1d1c895f30e9208
John <deejay.gvr@gmail.com>
2024-10-21 23:57:23 +0200

    creazione initial page

--- src/App.js
+++ src/App.js
@@ -4,23 +4,33 @@ import Sidebar from "./components/Sidebar";
 import Feed from "./pages/Feed";
 import Playlists from "./pages/Playlists";
 import Statistics from "./pages/Statistics";
-import "./App.css";
 import RegisterPage from "./components/RegisterPage";
 import LoginPage from "./components/LoginPage";
+import InitialPage from "./components/InitialPage"; // Assicurati che il percorso sia corretto
+import "./App.css";
 
 const App = () => {
+  const isAuthenticated = false; // Questo dovrebbe essere dinamico, basato sullo stato di autenticazione dell'utente
+
   return (
     <div className="app">
-      <Sidebar />
-      <div className="main-content">
-        <Routes>
-          <Route path="/" element={<Feed />} />
-          <Route path="/playlists" element={<Playlists />} />
-          <Route path="/statistics" element={<Statistics />} />
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="/login" element={<LoginPage />} />
-        </Routes>
-      </div>
+      {!isAuthenticated ? (
+        // Se l'utente non è autenticato, mostra la pagina iniziale
+        <InitialPage />
+      ) : (
+        <>
+          <Sidebar />
+          <div className="main-content">
+            <Routes>
+              <Route path="/" element={<Feed />} />
+              <Route path="/playlists" element={<Playlists />} />
+              <Route path="/statistics" element={<Statistics />} />
+              <Route path="/register" element={<RegisterPage />} />
+              <Route path="/login" element={<LoginPage />} />
+            </Routes>
+          </div>
+        </>
+      )}
     </div>
   );
 };
---
+++ src/components/InitialPage.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './InitialPage.css';
+
+const InitialPage = () => {
+  return (
+    <div className="initial-container">
+      <div className="overlay"></div>
+      <div className="content">
+        <h1 className="title">Benvenuti su EpicMusic</h1>
+        <p className="subtitle">Inizia subito a esplorare la tua musica preferita</p>
+        <div className="buttons-container">
+          <Link to="/register" className="button register-button">Registrati</Link>
+          <Link to="/login" className="button login-button">Login</Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default InitialPage;
