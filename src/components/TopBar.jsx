14be0fd2374f86123df209a2a22538b56db9c47a
John <deejay.gvr@gmail.com>
2024-10-29 14:31:13 +0100

    topbar logo

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Container, Navbar, Form, FormControl, Button, Dropdown, Col, Row } from "react-bootstrap";
 import { Link, useNavigate } from "react-router-dom";
 import "../styles/topbar.css";
+import logoImage from "../assets/images/logo.png";
 
 const TopBar = ({ onLogout, onSearch }) => {
   const [searchTerm, setSearchTerm] = useState("");
@@ -15,20 +16,20 @@ const TopBar = ({ onLogout, onSearch }) => {
     e.preventDefault();
     if (searchTerm.trim()) {
       onSearch(searchTerm);
-      navigate("/search"); // Naviga alla pagina dei risultati della ricerca
+      navigate("/search");
     }
   };
 
   return (
-    <Navbar bg="dark" variant="dark" fixed="top" className="top-bar">
+    <Navbar fixed="top" className="top-bar">
       <Container fluid>
         <Row className="align-items-center" style={{ width: "100%" }}>
           {/* Colonna sinistra - Logo */}
           <Col xs={2} className="d-flex align-items-center" style={{ paddingLeft: '10%' }}>
-  <Navbar.Brand as={Link} to="/">
-    EpicMusic
-  </Navbar.Brand>
-</Col>
+            <Navbar.Brand as={Link} to="/">
+              <img src={logoImage} alt="EpicMusic Logo" style={{ height: '35px', width: '150px' }} />
+            </Navbar.Brand>
+          </Col>
 
           {/* Colonna centrale - Searchbar */}
           <Col xs={7} className="d-flex justify-content-center" style={{ paddingLeft: '15%' }}>
@@ -40,7 +41,7 @@ const TopBar = ({ onLogout, onSearch }) => {
                 value={searchTerm}
                 onChange={handleSearchChange}
               />
-              <Button variant="outline-success" type="submit">
+              <Button variant="outline-info" type="submit">
                 Search
               </Button>
             </Form>
@@ -48,24 +49,24 @@ const TopBar = ({ onLogout, onSearch }) => {
 
           {/* Colonna destra - Profilo utente */}
           <Col xs={3} className="d-flex justify-content-end" style={{ paddingRight: '10%' }}>
-  <Dropdown align="end">
-    <Dropdown.Toggle variant="secondary" id="dropdown-avatar" className="avatar-dropdown">
-      <img
-        src="https://source.unsplash.com/random/50x50/?profile"
-        alt="Profile"
-        className="rounded-circle avatar"
-      />
-    </Dropdown.Toggle>
+            <Dropdown align="end">
+              <Dropdown.Toggle variant="secondary" id="dropdown-avatar" className="avatar-dropdown">
+                <img
+                  src="https://source.unsplash.com/random/50x50/?profile"
+                  alt="Profile"
+                  className="rounded-circle avatar"
+                />
+              </Dropdown.Toggle>
 
-    <Dropdown.Menu>
-      <Dropdown.Item as={Link} to="/account">Account</Dropdown.Item>
-      <Dropdown.Item as={Link} to="/profile">Profile</Dropdown.Item>
-      <Dropdown.Item as={Link} to="/settings">Settings</Dropdown.Item>
-      <Dropdown.Divider />
-      <Dropdown.Item onClick={onLogout}>Logout</Dropdown.Item>
-    </Dropdown.Menu>
-  </Dropdown>
-</Col>
+              <Dropdown.Menu>
+                <Dropdown.Item as={Link} to="/account">Account</Dropdown.Item>
+                <Dropdown.Item as={Link} to="/profile">Profile</Dropdown.Item>
+                <Dropdown.Item as={Link} to="/settings">Settings</Dropdown.Item>
+                <Dropdown.Divider />
+                <Dropdown.Item onClick={onLogout}>Logout</Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+          </Col>
         </Row>
       </Container>
     </Navbar>
